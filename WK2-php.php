58c86bbda70047747932412fc86f9e9428c7f8ea
Mayeda-Mark <markrmayeda@gmail.com>
2018-09-28 09:15:02 -0700

    initial commit

---
+++ WK2-php.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Wk 2 Team Activity</title>
+		<link rel="stylesheet" type="text/css" href="ta02.css">
+		<script type="text/javascript" src="ta02.js"></script>
+	</head>
+<body>
+	<div id="div1" class="div-first hover-bold"> This is the first one. </div>
+	<div id="div2" class="div-later hover-bold"> This is the second one. </div>
+	<div id="div3" class="div-later hover-bold"> This is the third one. </div>
+
+	<button onclick="clickMe();">Click Me!</button> <br />
+
+	<label for="txtColor">Color:</label> <input type="text" id="txtColor" placeholder="#cccccc"> <br />
+
+	<button onclick="changeColor();">Change Color</button>
+</body>
+</html>
